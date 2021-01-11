d97da950901ed6a378e63411508324fca84acd2b
CB <cadeburkett@gmail.com>
2021-01-10 17:57:59 -0800

    added questions array to index.js

--- Develop/index.js
+++ Develop/index.js
@@ -5,6 +5,46 @@ const fs = require('fs');
 
 // array of questions for user
 const questions = [
+    {
+        name: 'title',
+        type: 'input',
+        message: 'Insert Title',
+    },
+    {
+        name: 'description',
+        type: 'input',
+        message: 'Insert Description',
+    },
+    {
+        name: 'installation',
+        type: 'input',
+        message: 'Steps to install',
+    },
+    {
+        name: 'usage',
+        type: 'input',
+        message: 'How to use',
+    },
+    {
+        name: 'credits',
+        type: 'input',
+        message: 'List collaborators and third-party contributions',
+    },
+    {
+        name: 'license',
+        type: 'input',
+        message: 'List licensing (Githib, MIT, Etc.)',
+    },
+    {
+        name: 'tests',
+        type: 'input',
+        message: 'Test info',
+    },
+    {
+        name: 'questions',
+        type: 'input',
+        message: 'Insert contact for follow up questions',
+    },
 
 ];
 
